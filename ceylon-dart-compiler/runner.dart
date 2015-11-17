54e216693568cccad9d1c35d399c6d585454cd6e
John Vasileff <john@vasileff.com>
2015-11-17 17:43:52 -0500

    commit the runner.dart adhoc script runner file

---
+++ ceylon-dart-compiler/runner.dart
@@ -0,0 +1,18 @@
+library runnerLib;
+
+import "dart:core" as $dart$core;
+import "../ceylon-dart-language/build/modules-dart/main/ceylon/language/1.2.0-DP1-SNAPSHOT/ceylon.language-1.2.0-DP1-SNAPSHOT.dart" as $ceylon$language;
+
+void main() { run(); }
+
+//////////////////////////////////
+//
+// Paste compiled dart code below
+//
+//////////////////////////////////
+
+void $package$run() {
+    $ceylon$language.print($ceylon$language.String.instance("hello"));
+}
+
+void run() => $package$run();
