9c155bd835b5ea46d9558b498d11ed77220bb186
John Vasileff <john@vasileff.com>
2016-10-10 18:27:09 -0400

    sync main.dart and main2.dart

--- ceylon-dart-model-gen/main2.dart
+++ ceylon-dart-model-gen/main2.dart
@@ -38,7 +38,6 @@ const keyFlags         = "\$ff";
 const keyDefault       = "def";
 const keyNamed         = "nam";
 const keyDynamic       = "dyn";
-const keyStatic        = "sta";
 
 const keyNativeDart   = "\$mod-native-dart";
 
@@ -65,8 +64,11 @@ Map<String, int> annotationBits = {
   "abstract" : 1 << 8,
   "annotation" : 1 << 9,
   "variable" : 1 << 10,
-  "serializable" : 1 << 11
+  "serializable" : 1 << 11,
+  "static" : 1 << 12
 };
+
+TypeMirror voidType = currentMirrorSystem().voidType;
 
 Set<LibraryMirror> allowedLibraries;
 
@@ -269,15 +271,18 @@ Map<String, Object> classToInterfaceMap(ClassMirror cm, TypeMirror from) {
         // exclude dart._internal.EfficientLength and others?
         && MirrorSystem.getName(t.owner.qualifiedName) != "dart._internal";
     })
-    // don't erase in typeToMap...
-    .map((m) => typeToMap(m, from, false, false)).toList();
+    // - don't erase in typeToMap...
+    // - contravariant (i.e. dynamic=Nothing) type arguments for satisfied
+    //   types! The assumption is that most type parameters are covariant,
+    //   i.e. producers, and we use Nothing for return types.
+    .map((m) => typeToMap(m, from, false, false, false)).toList();
 
   // print("satisfies (i): " + map[keySatisfies].toString());
 
   // Also add the class as a static member class named ".Class"
   var classMap = classToClassMap(cm, from);
   classMap[keyName] = "Class";
-  classMap[keyStatic] = true;
+  classMap[keyPackedAnns] |= annotationBits["static"];
   map[keyClasses] = { "Class" : classMap };
 
   return map;
@@ -324,11 +329,11 @@ Map<String, Object> classToClassMap(ClassMirror cm, TypeMirror from) {
       keyName : "Basic"
   };
 
-  // satisfy the corresponding Ceylon interface, using all type paramters as
+  // satisfy the corresponding Ceylon interface, using all type parameters as
   // type arguments. Do not erase.
   map[keySatisfies] = [typeToMap(cm, from, false, false)];
   var typeArgs = typeArgumentMap(
-      cm.declarations.values.where((d) => d is TypeVariableMirror), from);
+      cm.declarations.values.where((d) => d is TypeVariableMirror), from, true);
   if (!typeArgs.isEmpty) {
     map[keySatisfies][0][keyTypeParams] = typeArgs;
   }
@@ -448,7 +453,7 @@ Map<String, Map<String, Object>> methodsToMap(
 Map<String, Object> variableToMap(VariableMirror mm, DeclarationMirror from,
     bool forceAbstract) {
   var map = new Map();
-  map[keyType] = typeToMap(mm.type, from); // TODO what about void?
+  map[keyType] = typeToMap(mm.type, from);
 
   if (!mm.isFinal && !mm.isConst) {
     map["\$set"] = { keyMetatype : metatypeSetter };
@@ -468,7 +473,7 @@ Map<String, Object> variableToMap(VariableMirror mm, DeclarationMirror from,
   }
 
   if (mm.isStatic) {
-    map[keyStatic] = true;
+    map[keyPackedAnns] |= annotationBits["static"];
   }
   return map;
 }
@@ -477,7 +482,8 @@ Map<String, Object> methodToMap(MethodMirror mm, DeclarationMirror from,
     bool forceAbstract) {
 
   var map = new Map();
-  map[keyType] = typeToMap(mm.returnType, from); // TODO what about void?
+  // contraviarant return (i.e. dynamic=Nothing)
+  map[keyType] = typeToMap(mm.returnType, from, false, true, false);
 
   if (mm.isGetter) {
     // FIXME Find a way to determine if a setter exists. We could search
@@ -511,7 +517,7 @@ Map<String, Object> methodToMap(MethodMirror mm, DeclarationMirror from,
   }
 
   if (mm.isStatic) {
-    map[keyStatic] = true;
+    map[keyPackedAnns] |= annotationBits["static"];
   }
   return map;
 }
@@ -525,15 +531,24 @@ List<Map<String, Object>> parametersToList(List<ParameterMirror> parameters,
       map["\$pt"] = "f";
       // Don't erase! Callable's are generic, so there is no automatic
       // type erasure.
-      map[keyType] = typeToMap(pt.returnType, from, false, false);
+
+      // For the return type of a function that is a parameter, we want
+      // covariant (i.e. dynamic=Anything) for maximum flexibility
+
+      // TODO: but what about when the function is a parameter of a
+      //    function of a parameter! parametersToList() needs a
+      //    covariant=true param.
+      map[keyType] = typeToMap(pt.returnType, from, false, false, true);
       map[keyMetatype] = metatypeParameter;
       map[keyName] = MirrorSystem.getName(pm.simpleName);
+      // TODO these param types should be contravariant (i.e. Nothing)...
       List plist = parametersToList(pt.parameters, from);
       if (plist.isNotEmpty) {
         map[keyParams] = [plist];
       }
     }
     else {
+      // covariant (i.e. dynamic=Anything)
       map[keyType] = typeToMap(pm.type, from);
       map[keyMetatype] = metatypeParameter;
       map[keyName] = MirrorSystem.getName(pm.simpleName);
@@ -552,16 +567,17 @@ List<Map<String, Object>> parametersToList(List<ParameterMirror> parameters,
 }
 
 List<Map<String, Object>> typeArgumentMap(
-    Iterable<DeclarationMirror> typeArguments, DeclarationMirror from) {
+    Iterable<DeclarationMirror> typeArguments, DeclarationMirror from, bool isCovariant) {
   return typeArguments.where((tm) => tm is TypeMirror).map((tm) {
-    var map = typeToMap(tm, from, false, false); // keyModule, keyName
+    var map = typeToMap(tm, from, false, false, isCovariant); // keyModule, keyName
     map[keyMetatype] = metatypeTypeParameter;
     return map;
   }).toList();
 }
 
 Map<String, Object> typeToMap(
-    TypeMirror tm, DeclarationMirror from, [bool isClass = false, bool erase = true]) {
+    TypeMirror tm, DeclarationMirror from,
+      [bool isClass = false, bool erase = true, bool isCovariant = true]) {
 
   // FIXME tm is null sometimes
 
@@ -644,7 +660,7 @@ Map<String, Object> typeToMap(
       map[keyModule] = tmModuleName;
     }
 
-    var tam = typeArgumentMap(tm.typeArguments, from);
+    var tam = typeArgumentMap(tm.typeArguments, from, isCovariant);
     if (tam.isNotEmpty) {
       map[keyTypeParams] = tam;
     }
@@ -653,10 +669,15 @@ Map<String, Object> typeToMap(
     // TODO make sure this works (type is type parameter)
     map[keyName] = MirrorSystem.getName(tm.simpleName);
   }
+  else if (tm == voidType) {
+    map[keyModule] = "\$";
+    map[keyName] = "Anything";
+    map[keyPackage] = "\$";
+  }
   else {
     // it must be dynamic
     map[keyModule] = "\$";
-    map[keyName] = "Anything";
+    map[keyName] = isCovariant ? "Anything" : "Nothing";
     map[keyPackage] = "\$";
   }
   return map;
