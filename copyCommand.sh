a0d08dfe25e27b1d5d8a2317800f83c0df69c371
John Vasileff <john@vasileff.com>
2017-06-05 12:28:00 -0400

    Add version to copyCommand

--- copyCommand.sh
+++ copyCommand.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-OUTPUT_REPOSITORY=/tmp/modules
+OUTPUT_REPOSITORY="/tmp/modules"
+VERSION="1.3.2-DP4"
 
 ceylon copy --verbose=files --all --scripts \
     --no-default-repositories \
     --rep ceylon-dart-cli/build/modules-jvm/main \
     --rep ceylon-dart-compiler/build/modules-jvm/main \
     --out $OUTPUT_REPOSITORY \
-    com.vasileff.ceylon.dart.compiler \
-    com.vasileff.ceylon.dart.cli
+    com.vasileff.ceylon.dart.compiler/$VERSION \
+    com.vasileff.ceylon.dart.cli/$VERSION
 
